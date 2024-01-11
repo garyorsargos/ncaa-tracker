fe8b775ea77c77b5ae8c1fdf3bfcc41d63737d90
Gary Orsargos <gary.orsargos@yahoo.com>
2024-01-11 15:37:00 -0500

    Added bot intentions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-const {Clients, IntentsBitField} = require(discord.js);
+const {Client, IntentsBitField} = require(discord.js);
+
+const client = new Client({
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+	]
+}) 
